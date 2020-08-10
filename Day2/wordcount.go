b03d2d20cfe4cb958bc92707f41b91258792ddde
Piyarach Saeoui <piyarach.s@kbtg.tech>
2020-08-10 15:49:08 +0700

    Update wordcount.go

--- Day2/wordcount.go
+++ Day2/wordcount.go
@@ -22,7 +22,7 @@ func WordCount(s string) map[string]int {
 }
 
 func main() {
-	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck"
+	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck."
 	w := WordCount(s)
 	fmt.Println(w)
 }
